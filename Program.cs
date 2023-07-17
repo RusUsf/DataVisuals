76850514af5a1f379935653db5c37733c79639c7
RusUsf <ruslan@usf.edu>
2023-07-16 20:51:24 -0400

    Added Styling to the Dashboard and Date to the file name, Dashboard opens automatically!

--- Program.cs
+++ Program.cs
@@ -31,31 +31,50 @@ class Program
             connection.Close();
         }
 
-        ExcelPackage.LicenseContext = LicenseContext.NonCommercial; // set the license context for your application
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
-        using (ExcelPackage package = new ExcelPackage(new FileInfo(@"C:\Users\rus_d\OneDrive\Desktop\Coding\Dotnet\CSharp\DataVisuals\Output\DowCounts.xlsx")))
+        // Include formatted date in filename
+        string formattedDate = DateTime.Now.Date.ToString("MMMM_d_yyyy"); // e.g., 'January_1_2023'
+        string filePath = $@"C:\Users\rus_d\OneDrive\Desktop\Coding\Dotnet\CSharp\DataVisuals\Output\DowCounts_{formattedDate}.xlsx";
+
+        using (ExcelPackage package = new ExcelPackage(new FileInfo(filePath)))
         {
-            ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("ChartData");
-            worksheet.Cells["A1"].Value = "Day of Week";
-            worksheet.Cells["B1"].Value = "Count";
+            ExcelWorksheet chartWorksheet = package.Workbook.Worksheets.Add("ChartData"); // Create chart worksheet first
+            ExcelWorksheet dataWorksheet = package.Workbook.Worksheets.Add("Data"); // Then create data worksheet
+
+            dataWorksheet.Cells["A1"].Value = "Day of Week";
+            dataWorksheet.Cells["B1"].Value = "Count";
 
             for (int i = 0; i < dayCounts.Count; i++)
             {
-                worksheet.Cells[i + 2, 1].Value = dayCounts[i].dow;
-                worksheet.Cells[i + 2, 2].Value = dayCounts[i].counts;
+                dataWorksheet.Cells[i + 2, 1].Value = dayCounts[i].dow;
+                dataWorksheet.Cells[i + 2, 2].Value = dayCounts[i].counts;
             }
 
-            var chart = worksheet.Drawings.AddChart("Sample Chart", eChartType.ColumnClustered);
-
-            var series = chart.Series.Add(worksheet.Cells["B2:B" + (dayCounts.Count + 1)], worksheet.Cells["A2:A" + (dayCounts.Count + 1)]);
+            var chart = chartWorksheet.Drawings.AddChart("Sample Chart", eChartType.ColumnClustered);
+            var series = chart.Series.Add(dataWorksheet.Cells["B2:B" + (dayCounts.Count + 1)], dataWorksheet.Cells["A2:A" + (dayCounts.Count + 1)]);
             series.Header = "Count";
 
             chart.Title.Text = "Count by Day of Week";
-            chart.SetPosition(1, 0, 3, 0);
-            chart.SetSize(800, 600);
+            chart.SetPosition(1, 0, 15, 0);
+            chart.SetSize(400, 300);
+
+            dataWorksheet.View.ShowGridLines = false;
+            chartWorksheet.View.ShowGridLines = false;
+
+            for (int i = 1; i <= dayCounts.Count + 1; i++)
+            {
+                dataWorksheet.Cells[i, 1].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                dataWorksheet.Cells[i, 1].Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.White);
+                dataWorksheet.Cells[i, 2].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                dataWorksheet.Cells[i, 2].Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.White);
+            }
 
             package.Save();
         }
+
+        // Open the Excel file
+        System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(filePath) { UseShellExecute = true });
     }
 }
 
